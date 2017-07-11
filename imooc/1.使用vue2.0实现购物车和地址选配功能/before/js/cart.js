68505b9743747e2302a232e69ad1e7a9cceb3e12
pangbooo <1838529223@qq.com>
2017-07-11 18:10:02 +0800

    711

--- imooc/1.使用vue2.0实现购物车和地址选配功能/before/js/cart.js
+++ imooc/1.使用vue2.0实现购物车和地址选配功能/before/js/cart.js
@@ -2,10 +2,13 @@ var vm = new Vue({
     el : '#app',
     data : {
         totalMoney : 0,
-        productList : []
+        productList : [],
+        checkAllFlag : false
     },
     filters : {
-
+        formatMoney : function(value){
+            return '￥' + value.toFixed(2)
+        }
     },
     mounted : function () {
         this.cartView();
@@ -17,6 +20,43 @@ var vm = new Vue({
                 _this.productList = res.data.result.list;
                 _this.totalMoney = res.data.result.totalMoney;
             })
+        },
+        changeMoney　: function(item,way){
+            if(way>0){
+                item.productQuantity++;
+            }else {
+                item.productQuantity--;
+                if(item.productQuantity<1){
+                    item.productQuantity = 1
+                }
+            }
+        },
+        selectProduct : function(item){
+            if( typeof item.checked == 'undefined'){
+                //Vue.set(item,'checked',true)//全局注册
+                this.$set(item,'checked',true); //局部注册
+            }else {
+                item.checked = !item.checked
+            }
+        },
+        checkAll : function(flag){
+            this.checkAllFlag = flag;
+            var _this = this;
+            if(this.checkAllFlag){
+                this.productList.forEach(function (value, index) {
+                    if(value.checked == 'undefined'){
+                        _this.$set(value,'checked',flag)
+                    }else {
+                        value.checked = flag
+                    }
+
+                })
+            }
         }
     }
+});
+
+//全局过滤器
+Vue.filter('money', function (value,type) {
+    return '￥' + value.toFixed(2) + type
 });
